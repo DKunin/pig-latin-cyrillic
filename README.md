afce03fd51b2c137c2386ca3c30a0f5cb67e3fd4
Dmitri Kunin <whitemarten@gmail.com>
2017-06-12 01:20:12 +0300

    master:Updates, fixes, and stuff

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# Title
+# Russian Pig Latin translator
 
-> This is an example file with default selections.
+> Переводчик на поросячью латынь [link](https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D1%80%D0%BE%D1%81%D1%8F%D1%87%D1%8C%D1%8F_%D0%BB%D0%B0%D1%82%D1%8B%D0%BD%D1%8C)
 
 ## Install
 
 ```
+    npm i pig-latin-cyrillic
 ```
 
 ## Usage
 
 ```
+import pigLatinCyr from 'pig-latin-cyrillic';
+
+console.log(pigLatinCyr('Проверка')); // 'Просовесеркаса'
+
+// Другие диалекты: «с» — «синий язык», «б» — «белый язык», «з» — «зелёный язык», «ф» — «фиолетовый язык», «к» — «кирпичный язык»
+
+console.log(pigLatinCyr('Проверка', 'б')); // 'Пробовеберкаба'
+
 ```
 
 ## Contribute
@@ -18,4 +27,4 @@ PRs accepted.
 
 ## License
 
-MIT © Richard McRichface
+MIT © Dmitri Kunin
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const APPROVED_DIALECTS = [ 'с', 'б', 'з', 'а', 'к', 'ф' ];
-const failString = 'Пожалуйста добавьте строку';
-const wrongDialectString = 'Допустимые диалекты: «с» — «синий язык», «б» — «белый язык», «з» — «зелёный язык», «ф» — «фиолетовый язык», «к» — «кирпичный язык»';
+const {
+    strings: { failString, wrongDialectString },
+    settings: { approvedDialects }
+} = require('./config');
 
 function pigLatin(input, dialect = 'с') {
     if (!input || typeof input !== 'string') {
         return failString;
     }
     dialect = dialect.toLowerCase();
-    if (dialect && !APPROVED_DIALECTS.includes(dialect)) {
+    if (dialect && !approvedDialects.includes(dialect)) {
         return wrongDialectString;
     }
     return input.replace(/а|у|о|ы|и|э|я|ю|ё|е/gi, `$&${dialect}$&`);
